756fe058d8cfdc938aa5b8fa55f4249f27081d25
svyatoslavvdovichenko <vdovichenko.fortech@gmail.com>
2023-05-24 18:51:05 +0300

    [FEATURE] Implement 404 page and error boundary

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,28 +1,28 @@
 import Header from 'components/Header';
 import { BeersTable } from 'pages/BeersTable/BeersTable';
+import { NotFound } from 'pages/NotFound/NotFound';
 import { Outlet, createBrowserRouter } from 'react-router-dom';
 
 //TODO implement error boundaries and 404 or redirect to page
 export const router = createBrowserRouter([
   {
-    path: 'beer',
+    path: '*',
     element: (
       <>
         <Header />
+        <NotFound />
+      </>
+    ),
+  },
+  {
+    path: 'table/*',
+    index: true,
+    element: (
+      <>
+        <Header />
+        <BeersTable />
         <Outlet />
       </>
     ),
-    children: [
-      {
-        path: 'table/*',
-        index: true,
-        element: (
-          <>
-            <BeersTable />
-            <Outlet />
-          </>
-        ),
-      },
-    ],
   },
 ]);
